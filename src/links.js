50c39e9f07eb66bcd12c34adfe4f9cf4e64efa09
chanyingchi <62597100+chanyingchi@users.noreply.github.com>
2020-12-26 14:21:00 +0100

    Update links.js
    links added

--- src/links.js
+++ src/links.js
@@ -1,5 +1,7 @@
 export default {
-  googleScholar: "https://google.scholar",
-  email: "mailto:",
-  researchGate: "",
+  googleScholar: "https://scholar.google.com/citations?hl=en&user=imgWSakAAAAJ",
+  email: "mailto:y.c.chan@rug.nl",
+  researchGate: "https://www.researchgate.net/profile/Ying_Chi_Chan",
+  github: "https://github.com/chanyingchi",
+  orcid: "https://orcid.org/0000-0002-7183-4411"
 };
