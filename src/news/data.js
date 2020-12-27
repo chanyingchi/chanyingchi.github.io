164f6f8c3d147cc740ad23f80df29224053bf741
chanyingchi <62597100+chanyingchi@users.noreply.github.com>
2020-12-27 12:15:38 +0100

    Update data.js

--- src/news/data.js
+++ src/news/data.js
@@ -6,7 +6,7 @@ const NEWS = [
     title:
       "Best Student Paper award at the 1st East Asian-Australasian Shorebird Science meeting!",
     text:
-      "Ginny Chan, a PhD student at the University of Groningen and the Royal Netherlands Insitute for Sea Research, has won the Best Student Paper award in the EAA Shorebird Science meeting held on 3-5 November 2020. The title of her talk is 'Applications of satellite tracking of shorebirds in coastal conservation'. Congratulations!",
+      "Ginny Chan, a PhD student at the University of Groningen and the Royal Netherlands Insitute for Sea Research, has been awarded the Best Student Paper award in the EAA Shorebird Science meeting held on 3-5 November 2020. The title of her talk is 'Applications of satellite tracking of shorebirds in coastal conservation'. Congratulations!",
     image: Image_EAAFSSM,
     url: "https://www.eaaflyway.net/1st-eaafssm-report/",
   },
