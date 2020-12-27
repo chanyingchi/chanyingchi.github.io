3f2b0a8cea1fee59befa29c4c52d3c1798a513b9
chanyingchi <62597100+chanyingchi@users.noreply.github.com>
2020-12-27 11:31:58 +0100

    Update data.js

--- src/news/data.js
+++ src/news/data.js
@@ -1,17 +1,22 @@
-import Image from '../home/profile_pic.jpg';
-
+import Image from '../best-student-paper_EAAFSSM_small.png';
+import Image_water from './
 const NEWS = [
   {
-    title: "Title",
+    title: "Best Student Paper award at the 1st East Asian-Australasian Shorebird Science meeting!",
     text:
-      "Building for nature: Preserving threatened bird habitat in port design",
+      "Ginny Chan, a PhD student at the University of Groningen and the Royal Netherlands Insitute for Sea Research, 
+    has won the Best Student Paper award in the EAA Shorebird Science meeting held on 3-5 November 2020. 
+    The title of her talk is 'Applications of satellite tracking of shorebirds in coastal conservation'. Congratulations!
+",
     image: Image,
+    url: "https://www.eaaflyway.net/1st-eaafssm-report/"
   },
   {
-    title: "Title",
+    title: "Our MDPI Water Paper has been chosen as 'Editor‘s Choice Article'",
     text:
-      "Building for nature: Preserving threatened bird habitat in port design",
-    image: Image,
+    <span>We are delighted to hear that our recent open-access publication entitled 'Building for Nature: Preserving Threatened Bird Habitat in Port Design' has been selected as <i>Editor's Choice Article</i> of Water journal.</span>,
+    image: Image_water,
+    url: "https://www.mdpi.com/2073-4441/12/8/2134"
   },
 ];
 
